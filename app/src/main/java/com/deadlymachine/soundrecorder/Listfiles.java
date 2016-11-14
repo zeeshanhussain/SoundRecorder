ab66f22b2e5a03cbd0409e53a0e4dcfbca816ed9
Neal Gosalia <nealgosalia@gmail.com>
2016-11-14 22:36:12 +0530

    Update Listfiles.java

--- app/src/main/java/com/deadlymachine/soundrecorder/Listfiles.java
+++ app/src/main/java/com/deadlymachine/soundrecorder/Listfiles.java
@@ -1,6 +1,7 @@
 package com.deadlymachine.soundrecorder;
 
 import android.app.ListActivity;
+import android.media.MediaPlayer;
 import android.os.Environment;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -12,22 +13,43 @@ import java.util.List;
 public class Listfiles extends ListActivity {
 
     private List<String> fileList = new ArrayList<String>();
+    private ListView listRecordings;
+    private MediaPlayer mMediaPlayer = null;
+    private boolean isPlaying=false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        listView = (ListView) findViewById(R.id.list_item);
+        final MediaPlayer myMediaPlayer = MediaPlayer.create(this, R.raw.audio);
         File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath().toString()+"/SoundRecorder");
-        ListDir(root);
-    }
-
-    void ListDir(File f) {
-        File[] files = f.listFiles();
+        File[] files = root.listFiles();
         fileList.clear();
         for (File file : files) {
             fileList.add(file.getPath());
         }
-        ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fileList);
-        setListAdapter(directoryList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                 this, 
+                 android.R.layout.list_item,
+                 fileList );
+         listRecordings.setAdapter(arrayAdapter);
     }
-
+    @Override  
+    protected void onListItemClick(ListView l, View v, int pos, long id) {  
+        super.onListItemClick(l, v, pos, id);
+        Object o = this.getListAdapter().getItem(pos);
+        String fileName = o.toString();
+        if(!isPlaying){
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(outputFile);
+            mMediaPlayer.setVolume(7.0f, 7.0f);
+            mMediaPlayer.prepare();
+            isPlaying=true;
+            mMediaPlayer.start();
+            onMediaPlayerCompletion();
+        } else{
+            isPlaying=false;
+            mMediaPlayer.stop();
+        }
+    }  
 }
